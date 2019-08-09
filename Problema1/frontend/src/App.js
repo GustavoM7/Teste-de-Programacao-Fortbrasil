10ec2b4c39722b2a92667bffa67bbca43af40e27
GustavoM7 <gustavosmarquesf@gmail.com>
2019-08-08 22:49:28 -0300

    Interface inicial

--- Problema1/frontend/src/App.js
+++ Problema1/frontend/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>GERENCIAMENTO DE ESTABELECIMENTOS</h2>
+          <p>BEM-VINDO(A)</p>
+
+          <form className='App-form'>
+            <label htmlFor='userInput'>USUÁRIO:</label>
+            <input className='App-input' id='userInput' type='text'/>
+
+            <label htmlFor='passwordInput'>SENHA:</label>
+            <input className='App-input' id='passwordInput' type='password'/>
+
+            <button className='App-button' type='submit'>AUTENTICAR</button>
+            <button className='App-button' type='submit'>CRIAR CONTA</button>
+          </form>
+          
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
